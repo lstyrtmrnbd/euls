1835c7053c9f89b428b1bfd16b69d65a08f69245
Marisa Kirisame <soymilk@airmail.cc>
2018-03-26 20:36:29 -0400

    Solved largest-product-in-a-series

--- euls.lisp
+++ euls.lisp
@@ -324,13 +324,20 @@
 ;; e.g. (sum 10 #'identity) ==> 55
 ;;      (sum 10 (lambda (i) (* i i))) ==> 385
 ;;
-;; Note 'i from 0 below n'
+;; Note it iterates n times but starts at 0
 (defun sum (n fun)
   "Sum 'n' numbers generated from 'fun'."
   (let ((sum 0))
     (loop for i from 0 below n do
          (setf sum (+ sum (funcall fun i))))
     sum))
+
+(defun product (n fun)
+  "Product of 'n' numbers generated from 'fun'."
+  (let ((prod 1))
+    (loop for i from 0 below n do
+         (setf prod (* prod (funcall fun i))))
+    prod))
 
 (defun generic-sum (n sum-proc fun-proc)
   "Compose by 'sum' 'n' numbers from 'fun'. 'sum' must take 2 arguments and compose, 'fun' must take 1 argument and generate."
@@ -342,8 +349,18 @@
 ;;; Sum square difference: (- (expt (sum 100 #'identity) 2) (sum 100 (lambda (i) (* i i))))
 ;;; - the '100's here should be 101 after changes to 'sum'
 
-;;; Input for "Largest product in a series":
-;; 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
-;; that's 1000 digits
+(defun largest-product-in-a-series (series n)
+  "Find the largest product of 'n' sequential digits in 'series' of digits."
+  (let ((seli (num-to-list series))
+        (cursub nil)
+        (curprod 0)
+        (greatest 0))
+    (loop for i from 0 below (- (length seli) n) do
+         (setf cursub (subseq seli i (+ n i)))
+         (setf curprod (product (length cursub)
+                           (lambda (i) (nth i cursub))))
+         (when (> curprod greatest)
+           (setf greatest curprod)))
+    greatest))
 
 
